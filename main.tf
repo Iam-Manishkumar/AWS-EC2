20c662716171be27151532c9f29d0d6d2aeae990
Manish Kumar <manishkumaar69@gmail.com>
2024-01-24 19:48:20 +0100

    RT's Creation

--- main.tf
+++ main.tf
@@ -43,3 +43,18 @@ resource "aws_subnet" "subnet" {
   }
 }
 
+resource "aws_route_table" "rt" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.ig.id}"
+  }
+}
+
+resource "aws_route_table_association" "rta" {
+  subnet_id      = "${aws_subnet.subnet.id}"
+  route_table_id = "${aws_route_table.rt.id}"
+}
+
+
